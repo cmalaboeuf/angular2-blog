d987b3065dbc806bbd6c4ccb2a2a481d4e38fa57
cmalaboeuf <christophe.malaboeuf@gmail.com>
2017-06-16 17:16:39 +0200

    Add users api

--- app-node/routes/api.js
+++ app-node/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var posts = require('./posts');
 var tags = require('./tags');
+var users = require('./users');
 
 /**
  * API Posts
@@ -21,5 +22,9 @@ router.post('/tags', tags.newTag);
 
 router.put('/tags/:id', tags.editTag);
 router.delete('/tags/:id',tags.deleteTag);
+
+router.get('/users/me', users.getMe);
+router.get('/users',users.getAll);
+router.get('/users/:id',users.getById);
 
 module.exports = router;
