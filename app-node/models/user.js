c39f0803133fecc39f9c4a8d9e5ffc8ebef5beab
cmalaboeuf <christophe.malaboeuf@gmail.com>
2017-06-18 23:30:58 +0200

    Add edit user in api. Update on user works but pre fct doesn't work

--- app-node/models/user.js
+++ app-node/models/user.js
@@ -33,6 +33,28 @@ UserSchema.pre('save', function (next) {
   }
 });
 
+UserSchema.pre('findOneAndUpdate', function (next) {
+  var user = this;  
+  user.comparePassword(user.password,function(err,isMatch){
+
+    if(!isMatch){
+      bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+          return next(err);
+        }
+        bcrypt.hash(user.password, salt, function (err, hash) {
+          if (err) {
+            return next(err);
+          }
+          user.password = hash;
+          console.error(user.password);
+          next();
+        });
+      });
+    }
+   });
+});
+
 UserSchema.methods.comparePassword = function (passw, cb) {
   bcrypt.compare(passw, this.password, function (err, isMatch) {
     if (err) {
--- app-node/routes/users.js
+++ app-node/routes/users.js
@@ -14,8 +14,27 @@ var userApi = {
       return res.send({'data':data});
     });
   },
-  getMe : (req,res) => {
-    return res.send({'data':req.user});
+  getMe : (req,res,next) => {
+    return res.send({"user":req.user});
+  },
+  editUser: (req, res,next) => {
+    var id = req.params.id;    
+    if(!req.body.name || ! req.body.password || !req.body.name && !req.body.password){     
+      res.status(400);
+      next();
+    }
+    else{
+      User.findOneAndUpdate({
+        _id: id
+      }, {
+        $set: {
+          name: req.body.name || '',
+          password: req.body.password || '',  
+        }}).exec();
+      res.status(200);
+      return res.send({});
+      
+    }
   }
 };
 module.exports = userApi;
