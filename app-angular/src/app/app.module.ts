242c3a3f710c814e55c30ca49feb3ff53f1a1679
cmalaboeuf <christophe.malaboeuf@gmail.com>
2017-06-21 15:33:21 +0200

    Add formGroup to manage newPassword form

--- app-angular/src/app/app.module.ts
+++ app-angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -53,6 +53,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     RouterModule.forRoot(routes),
     NgbModule.forRoot(),
--- app-angular/src/app/user/user.component.ts
+++ app-angular/src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from './user.service';
 import { User } from './Model/User';
+import {FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -13,13 +14,19 @@ import { User } from './Model/User';
 export class UserComponent implements OnInit {
   private id: string;
   private sub: any;
-  constructor(private route: ActivatedRoute,private userService :UserService) { }
+
+  passwordGroup : FormGroup;
+  constructor(private route: ActivatedRoute,private userService :UserService, fb:FormBuilder) {
+    this.passwordGroup = fb.group({
+      'newPassword':"",
+      "newPassword2" : ""
+    })
+  }
   private currentUser:User;
+
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-      console.log(params);
-      this.id = params['id'] + ""; // (+) converts string 'id' to a number
-       // In a real app: dispatch action to load the details here.
+      this.id = params['id'];
       this.userService.getOne(this.id).subscribe(res=>{
         this.currentUser = res['data'] as User;
       });
@@ -31,9 +38,17 @@ export class UserComponent implements OnInit {
     this.userService.update(this.currentUser).subscribe(res=>{return this.currentUser});
   }
 
+  updatePassword(){
+    if(this.passwordGroup.controls['newPassword'].value === this.passwordGroup.controls['newPassword2'].value){
+      this.currentUser.password = this.passwordGroup.controls['newPassword2'].value;
+      this.userService.update(this.currentUser).subscribe(res=>{return this.currentUser});
+    }
+    else{
+      this.passwordGroup.disabled;
+    }
+  }
+
   ngOnDestroy(){
     this.sub.unsubscribe();
   }
-
-
 }
