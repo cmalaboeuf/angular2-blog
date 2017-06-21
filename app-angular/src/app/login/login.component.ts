eaf5af23497788f09976b74a95d9fbf536daecd0
cmalaboeuf <christophe.malaboeuf@gmail.com>
2017-06-21 16:57:12 +0200

    Add formGroup on login form

--- app-angular/src/app/login/login.component.ts
+++ app-angular/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import {Router} from '@angular/router';
+import {FormGroup, FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,19 +10,21 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   public myservice:AuthService;
-  public usercreds: any;
-  public router : Router;
-  constructor(authservice:AuthService,router:Router) {
+  public usercreds: FormGroup;
+  constructor(authservice:AuthService,private router:Router,fb:FormBuilder) {
     this.myservice = authservice;
-    this.usercreds = {
-      email: '',
-      password: ''
-    }
-    this.router = router;
+    this.usercreds = fb.group({
+      "email": [null, Validators.required],
+      "password": [null, Validators.required]
+    });
   }
 
-  clickLogin(usercreds) {
-    this.myservice.login(usercreds).then(data => {
+  clickLogin() {
+    let user:Object = {
+      email:this.usercreds.controls.email.value,
+      password:this.usercreds.controls.password.value,
+    }
+    this.myservice.login(user).then(data => {
       if (data){
         this.router.navigate(["dashboard/newpost"]);
         return;
