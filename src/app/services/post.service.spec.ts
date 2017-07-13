d24b10bbbbfaf35f9babc4d68fcf896264dd6727
cmalaboeuf <christophe.malaboeuf@gmail.com>
2017-07-13 15:34:48 +0200

    Add some field in test

--- src/app/services/post.service.spec.ts
+++ src/app/services/post.service.spec.ts
@@ -4,14 +4,14 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { PostService } from './post.service';
 import { HttpModule, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
-import { Post} from '../blog/post/Model/Post';
+import { Post } from '../blog/post/Model/Post';
 
 
 describe('PostService', () => {
   const API_URL: String = 'http://example.com';
   beforeEach(() => {
     TestBed.configureTestingModule({
-       imports: [HttpModule],
+      imports: [HttpModule],
       providers: [
         PostService,
         { provide: XHRBackend, useClass: MockBackend }
@@ -22,17 +22,15 @@ describe('PostService', () => {
   it('should return an Observable<Array<Post>>', inject([PostService, XHRBackend], (postService: PostService, mockBackend: MockBackend) => {
     const mockResponse = {
       data: [
-        { title: 'Post 1'},
-        { title: 'Post 2'},
+        { title: 'Post 1', url: 'posts-1', content: 'This is a content', date: Date.now()},
+        { title: 'Post 2', url: 'posts-2', content: 'This is a content', date: Date.now() },
       ]
     };
-
     mockBackend.connections.subscribe((connection) => {
-          connection.mockRespond(new Response(new ResponseOptions({
-            body: mockResponse
-          })));
-        });
-
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: mockResponse
+      })));
+    });
     postService.getAll().subscribe((posts: any) => {
       expect(posts.data.length).toBe(2);
       expect(posts.data[0].title).toEqual('Post 1');
